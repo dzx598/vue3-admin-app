37e184b09d96365be73ba0fe02941fe8196149bc
duzhxiang <1499831531@qq.com>
2022-12-23 16:40:06 +0800

    feat: 侧边栏滑动

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,22 +1,25 @@
 <template>
-  <el-menu
-    class="sidebar-container-menu"
-    mode="vertical"
-    :default-active="activeMenu"
-    :background-color="scssVariables.menuBg"
-    :text-color="scssVariables.menuText"
-    :active-text-color="themeColor"
-    :collapse="sidebar.opened"
-    :collapse-transition="true"
-  >
-    <!-- 循环sidebar-item组件 -->
-    <sidebar-item
-      v-for="route in menuRoutes"
-      :key="route.path"
-      :item="route"
-      :base-path="route.path"
-    />
-  </el-menu>
+  <logo v-if="showLogo" :collapse="isCollapse" />
+  <scroll-panel>
+    <el-menu
+      class="sidebar-container-menu"
+      mode="vertical"
+      :default-active="activeMenu"
+      :background-color="scssVariables.menuBg"
+      :text-color="scssVariables.menuText"
+      :active-text-color="themeColor"
+      :collapse="sidebar.opened"
+      :collapse-transition="true"
+    >
+      <!-- 循环sidebar-item组件 -->
+      <sidebar-item
+        v-for="route in menuRoutes"
+        :key="route.path"
+        :item="route"
+        :base-path="route.path"
+      />
+    </el-menu>
+  </scroll-panel>
 </template>
 <script lang="ts" setup>
 import scssVariables from "@/styles/variables.module.scss"
@@ -41,4 +44,16 @@ const activeMenu = computed(() => {
 })
 const settingStore = useSettingsStore()
 const themeColor = computed(() => settingStore.settings.theme)
+// 是否显示logo
+const showLogo = computed(() => settingStore.settings.sidebarLogo)
+// 展开收起状态 稍后放store 当前是展开就让它收起
+const isCollapse = computed(() => store.sidebar.opened)
+const logoHeight = computed(() => (showLogo.value ? 50 : 0) + "px")
 </script>
+<style lang="scss" scoped>
+.sidebar-wrapper {
+  .sidebar-container-menu {
+    height: calc(100vh - v-bind(logoHeight));
+  }
+}
+</style>
